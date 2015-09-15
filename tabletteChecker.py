961270ad88cd03531788218fe9df1c5ac0bb0b2c
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-09-15 22:07:41 +0000

    Implement tablette checker

--- tabletteChecker.py
+++ tabletteChecker.py
@@ -1,6 +1,20 @@
 from serialChecker import SerialChecker
 
 class TabletteChecker(SerialChecker):
+     # 1k   2k   1k   2k
+     # 3k   3.9k 3k   3.9k
+     # 5.1k 7.5k 5.1k 7.5k
+    _target = ['C0', 'AD', 'C0', 'AD', 
+               'A2', '98', 'A2', '98', 
+               '8B', '79', '8B', '79']
+
     def test(self):
-        result = self.requestTablette()
-        return (1, result)
+        (statusCode, msg) = self.requestTablette()
+
+        if statusCode != 1:
+            return (statusCode, msg)
+
+        if self.getTomettesList() != self._target:
+            return (-1, "Diff:" + ''.join(self.getTomettesList()))
+
+        return (1, "Tomettes are identical !")
